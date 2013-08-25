d45ce21ca79f88ed083463ebd6ad20201836f673
test123 <=>
2013-08-26 00:10:14 +0200

    Add UI basic construction

--- DiskUsage/DiskUsage.cpp
+++ DiskUsage/DiskUsage.cpp
@@ -3,5 +3,22 @@
 #include "DiskUsage.h"
 
 DiskUsage::DiskUsage() {
+    pathLineEdit = new QLineEdit;
+    generateButton = new QPushButton(tr("Generate"));
 
+    pathGroupBoxLayout = new QHBoxLayout;
+    pathGroupBoxLayout->addWidget(pathLineEdit);
+    pathGroupBoxLayout->addWidget(generateButton);
+
+    pathGroupBox = new QGroupBox(tr("Path"));
+    pathGroupBox->setLayout(pathGroupBoxLayout);
+    pathGroupBox->setSizePolicy(QSizePolicy::Preferred, QSizePolicy::Fixed);
+
+    chartGroupBox = new QGroupBox(tr("Chart"));
+
+    mainLayout = new QVBoxLayout;
+    mainLayout->addWidget(pathGroupBox);
+    mainLayout->addWidget(chartGroupBox);
+
+    setLayout(mainLayout);
 }
--- DiskUsage/DiskUsage.h
+++ DiskUsage/DiskUsage.h
@@ -3,11 +3,26 @@
 
 #include <QWidget>
 
+class QGroupBox;
+class QHBoxLayout;
+class QLineEdit;
+class QPushButton;
+class QVBoxLayout;
+
 class DiskUsage : public QWidget {
     Q_OBJECT
 
 public:
     DiskUsage();
+
+private:
+    QGroupBox *pathGroupBox;
+    QLineEdit *pathLineEdit;
+    QPushButton *generateButton;
+    QGroupBox *chartGroupBox;
+
+    QHBoxLayout *pathGroupBoxLayout;
+    QVBoxLayout *mainLayout;
 };
 
 #endif
